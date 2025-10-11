d28da732b83e9e623ea75ab957d7839947fa386b
Alex Peirano <alexpeirano29@gmail.com>
2025-10-11 18:33:31 +0200

    dossier de con

--- app/.buildozer/android/platform/build-arm64-v8a/build/other_builds/sqlite3/arm64-v8a__ndk_target_21/sqlite3/obj/local/arm64-v8a/objs-debug/sqlite3/sqlite3.o.d
+++
@@ -1,2 +0,0 @@
-/Users/alexpeirano/Desktop/couad/fil_rouge/prototype/app/.buildozer/android/platform/build-arm64-v8a/build/other_builds/sqlite3/arm64-v8a__ndk_target_21/sqlite3/obj/local/arm64-v8a/objs-debug/sqlite3/sqlite3.o: \
-  /Users/alexpeirano/Desktop/couad/fil_rouge/prototype/app/.buildozer/android/platform/build-arm64-v8a/build/other_builds/sqlite3/arm64-v8a__ndk_target_21/sqlite3/jni/../sqlite3.c
